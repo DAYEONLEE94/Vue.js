2f267cd6683c424050d1af0d397b8c9cad2f8085
Daani <luv120920@naver.com>
2024-12-02 02:08:18 +0900

    Vue의 기본적인 구조와 흐름 이해 연습

--- 01/src/main.js
+++ 01/src/main.js
@@ -1,6 +1,20 @@
-import './assets/main.css'
+import { createApp, h} from 'vue'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp({
+    data(){
+        return{
+            message: 'Hello, Vue!',
+        };
+    },
+    methods: {
+        reverse(){
+            this.message = this.message.split('').reverse().join('');
+        },
+    },
+        render(){
+            return h('div', [
+                h('p', this.message),
+                h('button', { onClick: this.reverse}, 'Reverse'),
+            ]);
+        },
+}).mount('#app')
