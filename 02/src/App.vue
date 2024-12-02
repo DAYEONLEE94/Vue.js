d278f3ce4c7737d1c97cb6b476af05cbab490706
Daani <luv120920@naver.com>
2024-12-02 16:50:43 +0900

    v-if, v-else-if, v-else 디렉티브 사용

--- 02/src/App.vue
+++ 02/src/App.vue
@@ -2,20 +2,24 @@
 export default {
   data() {
     return {
-     message: '<h1>Hello, Vue JS!</h1>',
-     name: '철수',
-     age: 19,
-     className: 'red-color',
+     visible: true,
+     unvisible: false,
+     condition: 'F',
     };
   },
 };
 </script>
 <template>
- <h1 v-bind:class="className">{{ name }}</h1>
+ <h1 v-if="visible">이 요소는 렌더링 됩니다.</h1>
+ <h1 v-if="unvisible">이 요소는 렌더링되지 않습니다.</h1>
+
+ <h1 v-if="condition === 'A'">condition {{ condition }} 입니다.</h1>
+ <h1 v-else-if="condition === 'B'">condition {{ condition }} 입니다.</h1>
+ <h1 v-else-if="condition === 'C'">condition {{ condition }} 입니다.</h1>
+ <h1 v-else-if="condition === 'D'">condition {{ condition }} 입니다.</h1>
+ <h1 v-else="condition">condition이 없습니다.</h1>
+
 </template>
 
 <style>
-.red-color{
-  color: red;
-}
 </style>
