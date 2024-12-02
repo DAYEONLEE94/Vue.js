ae619d87188361af04d19f4f8219d5189782b2bb
Daani <luv120920@naver.com>
2024-12-02 14:45:54 +0900

    옵션스 API 데이터 정의하기

---
+++ 02/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'abc',
+      age: 19,
+    };
+  },
+};
+</script>
+<template>
+ <p>저의 이름은 {{ name }} 입니다.</p>
+ <p>저의 나이는 {{ age }} 입니다.</p>
+ {{ 10 * 20 * 30 }}
+ {{ name.toUpperCase() }}
+</template>
+<style></style>
