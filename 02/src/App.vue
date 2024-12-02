750bd85b7cd76cf6faf1e0fcad48c71446c0bba4
Daani <luv120920@naver.com>
2024-12-02 17:29:03 +0900

    v-for 예제

--- 02/src/App.vue
+++ 02/src/App.vue
@@ -2,16 +2,20 @@
 export default {
   data() {
     return {
-    fruits: ['apple', 'banana', 'orange'],
+    user: {
+      name : 'Jone',
+      age : 20,
+      gender: 'male',
+    }
     };
   },
 };
 </script>
 <template>
-  <h1>fruits 데이터 반복 결과</h1>
+  <h1>user 데이터 반복 결과</h1>
   <ul>
-    <li v-for="(value, index) in fruits" :key="index">
-       인덱스: {{ index }}, 값: {{ value }}
+    <li v-for="(value, key, index) in user" :key="index">
+        {{ index }}: {{ key }}: {{ value }}
     </li>
   </ul>
 </template>
