c7d70d778a1d71c03779d4f33828a3e848a19316
Daani <aekdusa94@gmail.com>
2024-12-02 16:31:55 +0900

    v-bind디렉티브 사용

--- 02/src/App.vue
+++ 02/src/App.vue
@@ -5,11 +5,17 @@ export default {
      message: '<h1>Hello, Vue JS!</h1>',
      name: '철수',
      age: 19,
+     className: 'red-color',
     };
   },
 };
 </script>
 <template>
- <h1 v-pre>{{ name }}</h1>
+ <h1 v-bind:class="className">{{ name }}</h1>
 </template>
-<style></style>
+
+<style>
+.red-color{
+  color: red;
+}
+</style>
