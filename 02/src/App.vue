211488949bbc315d4eb4595375c84a4608962f72
Daani <luv120920@naver.com>
2024-12-02 17:26:17 +0900

    v-for디렉티브 사용

--- 02/src/App.vue
+++ 02/src/App.vue
@@ -2,23 +2,18 @@
 export default {
   data() {
     return {
-     visible: true,
-     unvisible: false,
-     condition: 'F',
+    fruits: ['apple', 'banana', 'orange'],
     };
   },
 };
 </script>
 <template>
- <h1 v-if="visible">이 요소는 렌더링 됩니다.</h1>
- <h1 v-if="unvisible">이 요소는 렌더링되지 않습니다.</h1>
-
- <h1 v-if="condition === 'A'">condition {{ condition }} 입니다.</h1>
- <h1 v-else-if="condition === 'B'">condition {{ condition }} 입니다.</h1>
- <h1 v-else-if="condition === 'C'">condition {{ condition }} 입니다.</h1>
- <h1 v-else-if="condition === 'D'">condition {{ condition }} 입니다.</h1>
- <h1 v-else="condition">condition이 없습니다.</h1>
-
+  <h1>fruits 데이터 반복 결과</h1>
+  <ul>
+    <li v-for="(value, index) in fruits" :key="index">
+       인덱스: {{ index }}, 값: {{ value }}
+    </li>
+  </ul>
 </template>
 
 <style>
