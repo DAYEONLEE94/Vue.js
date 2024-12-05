47b78ad5ac27869a3fb697a454e247a3dd8b2ab3
Daani <luv120920@naver.com>
2024-12-05 15:35:26 +0900

    v-momo디렉티브로 렌더링 결과 저장하기

--- 03/src/App.vue
+++ 03/src/App.vue
@@ -2,20 +2,25 @@
 export default {
   data(){
     return {
-      number: 0
+      number1: 0,
+      number2: 0,
     };
   },
   methods: {
-    onClick(){
-      this.number++;
+    onClick1(){
+      this.number1++;
+    },
+    onClick2(){
+      this.number2++;
     },    
   },
 };
 </script>
 
 <template>
-  <h1 v-once>{{ number }}</h1>c
-  <button @click="onClick">증가</button>
+  <h1 v-memo="[number1, number2]">{{ number1 }} / {{number2}}</h1>
+  <button @click="onClick1">증가</button>
+  <button @click="onClick2">증가</button>
 </template>
 
 <style></style>
