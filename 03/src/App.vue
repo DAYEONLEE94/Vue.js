274d3859c9cd9e84b1862b3ccb624814345f5060
Daani <luv120920@naver.com>
2024-12-05 23:25:25 +0900

    체크박스 요소에서 값 가져오기

--- 03/src/App.vue
+++ 03/src/App.vue
@@ -1,15 +1,15 @@
 <script>
-  export default{
-    data(){
-      return{
-        message: '',
+  export default{ 
+    data() {
+      return {
+        fruits: ['banana', 'orange'],
       };
     },
+
     methods: {
       printData() {
-        console.log(this.message);      
-      }
-      
+        console.log(this.fruits);
+      },
     },
   }
 
@@ -17,9 +17,17 @@
 </script>
 
 <template>
-  <form id="loginCheck">
-   <textarea v-model="message"></textarea>
-    <button type="button" @click="printData">데이터출력</button>
+  <form id="loginForm">
+    <label for="banana">
+      <input type="checkbox" id="banana" v-model="fruits" value="banana"/>banana
+    </label>
+    <label for="orange">
+      <input type="checkbox" id="orange" v-model="fruits" value="orange"/>orange
+    </label>
+    <label for="apple">
+      <input type="checkbox" id="orange" v-model="fruits" value="apple"/>apple
+    </label>
+    <button type="button" @click="printData">확인</button>
   </form>
  
 </template>
