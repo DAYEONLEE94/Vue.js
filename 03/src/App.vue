6dc1455270a30eba8129c05ebf55cf5a590e74d3
Daani <luv120920@naver.com>
2024-12-10 00:08:59 +0900

    3장 예제문제

--- 03/src/App.vue
+++ 03/src/App.vue
@@ -1,9 +1,30 @@
-<script></script>
+<script>
+  export default{
+    data(){
+      return {
+        number: 0,
+      }
+    },
+    
+    methods: {
+      onclick1(number){
+        if(this.number < 10){
+          this.number++;
+        }
+      },
+      onclick2(number){
+        if(this.number > 0){
+          this.number--;
+        }
+      },
+    },
+  }
+</script>
 
 <template>
-  <h1>Sample Text</h1>
+  숫자 : {{ number }}
+  <button @click="onclick1">증가</button>
+  <button @click="onclick2">감소</button>
 </template>
-<style>
-/* @import './assets/main.css'; */
-@import "@/assets/main.css"
-</style>
+
+<style></style>
