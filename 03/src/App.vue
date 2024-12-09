147caeeb2b287e0647e95f37b17e075eb7adcb47
Daani <luv120920@naver.com>
2024-12-09 23:40:56 +0900

    v-bind디렉티브의 스타일 속성

--- 03/src/App.vue
+++ 03/src/App.vue
@@ -1,35 +1,16 @@
 <script>
-  export default{
+  export default {
     data(){
-      return { 
-        arr: [0,1,2],
-        obj: {name: 'chulsu'},
+      return{
+        color: 'blue',
+        style: 'italic',
       };
     },
-    watch: {
-      arr: {
-        handler(newValue, oldValue){
-          console.log(`old value : ${oldValue}`);
-          console.log(`new Value : ${newValue}`);
-        },
-        deep: true
-      },
-
-      obj: {
-        handler(newValue, oldValue){
-          console.log(`old value : ${JSON.stringify(oldValue)}`);
-          console.log(`new value : ${JSON.stringify(newValue)}`);
-        },
-        deep: true,
-      },
-    },
   }
+
 </script>
 
 <template>
-  <h1>{{arr}}</h1>
-  <h1>{{ obj }}</h1>
-  <button @click="arr.push(3)">배열 변경</button>  
-  <button @click="obj.age = 20">객체 변경</button>
+  <h1 v-bind:style="{color: color, fontStyle: style}">Sample Text</h1>
 </template>
 <style></style>
