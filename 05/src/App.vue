2528f91bef6ab0365caec9cfe73a7b19518eece8
Daani <luv120920@naver.com>
2024-12-16 23:47:26 +0900

    컴포넌트 사용자 정의 이벤트

--- 05/src/App.vue
+++ 05/src/App.vue
@@ -1,26 +1,17 @@
+
 <script>
-import FirstChild from "@/components/FirstChild.vue";
+import UserProfile from '@/components/UserProfile.vue';
 export default {
   components: {
-    FirstChild,
+    UserProfile,
   },
-  mounted() {
-    const childNum = this.$refs.child1.num;
-    const childDoubleNum = this.$refs.child1.doubleNum;
-    console.log(`메서드 호출 전: ${childNum}`); // 5 출력
-    console.log(`메서드 호출 전: ${childDoubleNum}`); // 10 출력
-    // DOM이 마운트되고 1초 뒤에 TheChild 컴포넌트의 increment() 메서드 호출
-
-    setTimeout(function() {
-      this.$refs.child1.increment();
-      console.log(`메서드 호출 전: ${childNum}`); // 6 출력
-      console.log(`메서드 호출 전: ${childDoubleNum}`); // 12 출력
-    }, 1000);
-
-    
+  methods: {
+    printHello() {
+      alert('안녕하세요!');
+    },
   },
 };
 </script>
 <template>
-  <FirstChild ref="child1" />
+  <UserProfile @print-hello="printHello" />
 </template>
