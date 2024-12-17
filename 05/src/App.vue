58552d8081c5f300176de942f1f40b197355f05c
Daani <luv120920@naver.com>
2024-12-17 19:15:53 +0900

    컴포넌트 - 혼합슬롯

--- 05/src/App.vue
+++ 05/src/App.vue
@@ -1,27 +1,26 @@
 <script>
-import DefaultLayout from '@/components/DefaultLayout.vue';
+import MixLayout from '@/components/MixLayout.vue';
 export default {
   components: {
-    DefaultLayout,
+    MixLayout,
   },
 };
 </script>
 <template>
-  <DefaultLayout>
-    <template v-slot:header>
+  <MixLayout>
+    <template #header>
       <header>
         <h1>Header</h1>
       </header>
     </template>
-    <template v-slot:main>
-      <main>
-        <h1>Main</h1>
-      </main>
-    </template>
-    <template v-slot:footer>
+    <!-- default -->
+    <main>
+      <h1>Main</h1>
+    </main>
+    <template #footer>
       <footer>
         <h1>Footer</h1>
       </footer>
     </template>
-  </DefaultLayout>
+  </MixLayout>
 </template>
