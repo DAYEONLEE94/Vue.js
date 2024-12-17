bf08c42a76eb38cc45741f97f04a3fb738e7c47c
Daani <luv120920@naver.com>
2024-12-17 19:16:34 +0900

    컴포넌트 -  슬롯에 기본값 적용

--- 05/src/App.vue
+++ 05/src/App.vue
@@ -1,26 +1,11 @@
 <script>
-import MixLayout from '@/components/MixLayout.vue';
+import CustomButton from '@/components/CustomButton.vue';
 export default {
   components: {
-    MixLayout,
+    CustomButton,
   },
 };
 </script>
 <template>
-  <MixLayout>
-    <template #header>
-      <header>
-        <h1>Header</h1>
-      </header>
-    </template>
-    <!-- default -->
-    <main>
-      <h1>Main</h1>
-    </main>
-    <template #footer>
-      <footer>
-        <h1>Footer</h1>
-      </footer>
-    </template>
-  </MixLayout>
+  <CustomButton />
 </template>
