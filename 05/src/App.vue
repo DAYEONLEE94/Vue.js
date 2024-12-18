ced3e84c6446c03b796111968f3eae31e7102a5a
Daani <luv120920@naver.com>
2024-12-18 23:43:31 +0900

    컴포넌트 - 슬롯의 범위

--- 05/src/App.vue
+++ 05/src/App.vue
@@ -1,20 +1,26 @@
 <script>
-import DynamicLayout from '@/components/DynamicLayout.vue';
+import BasicScope from '@/components/BasicScope.vue';
+import BasicScope2 from '@/components/BasicScope2.vue';
 export default {
   components: {
-    DynamicLayout,
+    BasicScope,
+    BasicScope2,
   },
   data() {
     return {
-      dynamicName: 'header',
+      message: 'parent',
+      count: 0,
     };
   },
 };
 </script>
 <template>
-  <DynamicLayout>
-    <template #[dynamicName]>
-      <h2>동적 슬롯</h2>
-    </template>
-  </DynamicLayout>
+  <BasicScope2>
+    <h1>범위: {{ message }} / {{ count }}</h1>
+  </BasicScope2>
 </template>
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
