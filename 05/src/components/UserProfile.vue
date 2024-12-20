bf43e4e9391329690d5e0b0c128ab8497aad3ace
Daani <luv120920@naver.com>
2024-12-20 19:46:24 +0900

    컴포넌트 - 이벤트 검증

--- 05/src/components/UserProfile.vue
+++ 05/src/components/UserProfile.vue
@@ -1,17 +1,15 @@
 <script>
-    export default {
-        methods : {
-            parentEventCall() {
-                this.$emit('print-hello');
-            }
+  export default {
+    emits: ['print-hello'],
+
+    methods: {
+        parentEventCall() {
+            this.$emit('onPrint-hello');
         }
     }
+}
 
 </script>
 <template>
-    인라인 핸들러 방식
-    <p><button @click="$emit('print-hello')">클릭</button></p>
-
-    메서드 핸들러 방식
-    <p><button @click="parentEventCall">클릭</button></p>
+    <button @click="parentEventCall">클릭</button>
 </template>
