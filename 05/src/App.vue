21807ef10f834cc83b6a78b19097dfd9f02e2081
Daani <luv120920@naver.com>
2024-12-24 17:37:08 +0900

    컴포넌트 - 이름이 있는 슬롯

--- 05/src/App.vue
+++ 05/src/App.vue
@@ -1,13 +1,32 @@
 <script>
 import CustomButton from '@/components/CustomButton.vue';
+import DefaultLayout from '@/components/DefaultLayout.vue';
 
 export default {
   components: {
     CustomButton,
+    DefaultLayout,
   },
 }
 </script>
 <template>
  <CustomButton>더블클릭</CustomButton>
+ <DefaultLayout>
+    <template v-slot:header>
+      <header>
+        <h1>Header</h1>
+      </header>
+    </template>
+    <template #main>
+      <main>
+        <h1>Main</h1>
+      </main>
+    </template>
+    <template #footer>
+      <footer>
+        <h1>Footer</h1>
+      </footer>
+    </template>
+ </DefaultLayout>
 
 </template>
